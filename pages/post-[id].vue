cc5b98c87e7a1fe0f420b910e2ed2ae44c000e4c
masamiti <azain4645@yahoo.co.jpgit push origin sec-02_componentsSlot>
2022-07-13 20:05:47 +0900

    first commit

---
+++ pages/post-[id].vue
@@ -0,0 +1,24 @@
+<!-- pages/post-[id].vue -->
+<script>
+  export default {
+    layout: "blog"
+  }
+</script>
+
+<script setup>
+  const route = useRoute()
+  const { data: post } = await useFetch(`https://jsonplaceholder.typicode.com/posts/${route.params.id}`, { pick: ['title', 'body'] })
+</script>
+
+<template>
+  <div>
+    <NuxtLink to="/">
+      <h1 class="m-4 hover:underline">Back to Home</h1>
+    </NuxtLink>
+    <!-- A <quote /> component should be added here later on -->
+    <article class="m-4 md:w-1/2 lg:w-1/3">
+      <h2 class="mb-2 capitalize text-2xl font-semibold">{{ post.title }}</h2>
+      <p>{{ post.body }}</p>
+    </article>
+  </div>
+</template>
