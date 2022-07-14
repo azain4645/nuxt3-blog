e5b0f352b4406e662c21f6611732bc3e04d6f738
masamiti <azain4645@yahoo.co.jpgit push origin sec-02_componentsSlot>
2022-07-14 10:56:15 +0900

    fortuneコンポーネントに切り出し

---
+++ components/fortune.vue
@@ -0,0 +1,15 @@
+<script setup>
+  const lucky = ref('')
+  const results = ['大吉', '吉', '大凶'];
+  const fortuneTest = () => {
+    const random = Math.floor( Math.random() * 3)
+    lucky.value = results[random]
+  }
+</script>
+
+<template>
+  <h2>おみくじ</h2>
+  <p>今日の運勢を占います。↓のボタンをクリックしてね</p>
+  <button @click="fortuneTest" class="m-2 py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700">今日の運勢</button>
+  <p>{{ lucky }}</p>
+</template>
