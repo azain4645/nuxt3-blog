38f436b816acc4a130a4353b766fe96bd02af480
masamiti <azain4645@yahoo.co.jpgit push origin sec-02_componentsSlot>
2022-07-14 15:55:56 +0900

    reactiveによる状態定義

--- pages/todoList.vue
+++ pages/todoList.vue
@@ -1,15 +1,23 @@
 <script setup lang="ts">
-  const todos = [
-        {content: 'テスト', created: '2020-04-30 17:00', state: '作業前'}, 
-        {content: 'コーディング', created: '2020-04-30 16:00', state: '作業中'},
-        {content: '環境構築', created: '2020-04-30 15:30', state: '完了'}
-      ]
+  interface Todo {
+    content: string;
+    created: string;
+    state: boolean;
+  }
+ 
+  let todos = reactive<Todo[]>([
+        {content: 'テスト', created: '2020-04-30 17:00', state: false}, 
+        {content: 'コーディング', created: '2020-04-30 16:00', state: false},
+        {content: '環境構築', created: '2020-04-30 15:30', state: true}
+      ])
+
+  let content = ref('')
 
   const addTodo = () => {
     todos.push({
       content: "追加テスト",
       created: "2020-04-30 12:30",
-      state: '作業前'
+      state: false
     })
   }
 </script>
@@ -18,7 +26,7 @@
   <div class="m-2">
     <h1 class="text-2xl mb-5">Todoリスト</h1>
     <div class="flex mb-5">
-      <input type="text" class="w-2/3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="タスクを入力してください">
+      <input v-model="content" type="text" class="w-2/3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="タスクを入力してください">
       <button @click="addTodo" class="ml-5 w-32 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700">追加</button>
     </div>
 
