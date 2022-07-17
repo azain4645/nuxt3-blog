f63f72ad4dcea76ca0bef6fed478c3774784268a
masamiti <azain4645@yahoo.co.jpgit push origin sec-02_componentsSlot>
2022-07-17 11:03:14 +0900

    validation途中

--- pages/todoList.vue
+++ pages/todoList.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { valueToNode } from '@babel/types';
+import { useField } from 'vee-validate'
+import * as yup from 'yup';
 
   type Todo = {
     content: string;
@@ -6,7 +9,7 @@
     isDone: boolean;
   }
  
-  const content = ref('')
+  // const content = ref('')
 
   const todos = ref<Todo[]>([
          {content: 'テスト', created: '2020-04-30 17:00', isDone: true}, 
@@ -23,14 +26,14 @@
     : todos.value.filter(todo => todo.isDone)
   )
 
-  const addTodo = () => content.value && (() => {
+  const addTodo = () => {
       todos.value.push({
         content: content.value,
         created: formatedNow(),
         isDone: false
       })
       content.value = "";
-  }) ()
+  }
 
   const formatedNow = () : string => {
     const d = new Date()
@@ -42,20 +45,60 @@
   }
 
   const dropItem = (index : number) => todos.value.splice(index, 1)
+
+  const { value: content, errorMessage: contentError, handleChange, meta } = useField<string>('content', yup.string().required().min(3))
 </script>
 
 <template>
   <div class="m-2">
     <h1 class="text-2xl mb-5">Todoリスト</h1>
-    <div class="flex mb-5">
-      <input v-model="content" type="text" class="w-2/3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="タスクを入力してください">
-      <button @click="addTodo" class="ml-5 w-32 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700">追加</button>
+    <div class="flex flex-wrap">
+      <input :value="content" @change="handleChange" type="text" class="w-2/3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="タスクを入力してください">
+      <button 
+        @click="addTodo" 
+        :disabled="!meta.valid"
+        :class="`${
+          meta.valid === true 
+          ? 'bg-blue-500 hover:bg-blue-700'
+          : 'bg-blue-300'
+        } ml-5 w-32  text-white font-semibold rounded-lg shadow-md`">
+          追加
+      </button>
     </div>
+    <p class="mb-5"> {{ contentError }}</p>
+    <p>valid:{{ meta.valid }}</p>
+    <p>dirty:{{ meta.dirty }}</p>
+    <p>initialValue:{{ meta.initialValue }}</p>
 
     <div class="Filter mb-5">
-      <button @click="mode = 'all'" v-bind:class="{'bg-gray-700' : mode === 'all', 'bg-gray-400' : mode !== 'all'}" class="ml-5 w-32 h-8 text-white font-semibold rounded-lg shadow-md hover:bg-gray-700">全て</button>
-      <button @click="mode = 'notYet'" v-bind:class="{'bg-gray-700' : mode === 'notYet', 'bg-gray-400' : mode !== 'notYet'}" class="ml-5 w-32 h-8  text-white font-semibold rounded-lg shadow-md hover:bg-gray-700">実施前</button>
-      <button @click="mode = 'done'" v-bind:class="{'bg-gray-700' : mode === 'done', 'bg-gray-400' : mode !== 'done'}" class="ml-5 w-32 h-8 text-white font-semibold rounded-lg shadow-md hover:bg-gray-700">実施後</button>
+      <button 
+       @click="mode = 'all'"
+       :class="`${
+        mode === 'all'
+        ? 'bg-gray-700'
+        : 'bg-gray-400'
+       } ml-5 w-32 h-8 text-white font-semibold rounded-lg shadow-md hover:bg-gray-700`">
+        全て
+      </button>
+
+      <button 
+       @click="mode = 'notYet'"
+       :class="`${
+        mode === 'notYet'
+        ? 'bg-gray-700'
+        : 'bg-gray-400'
+       } ml-5 w-32 h-8 text-white font-semibold rounded-lg shadow-md hover:bg-gray-700`">
+        実施前
+      </button>
+      
+      <button 
+       @click="mode = 'done'"
+       :class="`${
+        mode === 'done'
+        ? 'bg-gray-700'
+        : 'bg-gray-400'
+       } ml-5 w-32 h-8 text-white font-semibold rounded-lg shadow-md hover:bg-gray-700`">
+      実施後</button>
     </div>
     
 		<table class="w-full flex-row flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-lg my-5">
