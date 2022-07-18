96b3fa829a2b902287522699b3835dbc36002c69
masamiti <azain4645@yahoo.co.jpgit push origin sec-02_componentsSlot>
2022-07-18 20:44:59 +0900

    NuxtLink設定

--- layouts/blog.vue
+++ layouts/blog.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="flex flex-col w-full h-screen">
-    <header class="text-white bg-green-500 p-4">HEADER</header>
+    <header class="text-white bg-green-500 p-4 flex">
+      <h3 class="mr-5">HEADER</h3>
+      <ul class="text-left flex">
+        <NuxtLink :to="`/`">
+          <li class="mr-2">blog</li>
+        </NuxtLink>
+        <NuxtLink :to="`/counter`">
+          <li class="mr-2">counter</li>
+        </NuxtLink>
+        <NuxtLink :to="`/fortunePage`">
+          <li class="mr-2">fortune</li>
+        </NuxtLink>
+        <NuxtLink :to="`/todoList`">
+          <li class="mr-2">todoList</li>
+        </NuxtLink>
+      </ul>
+    </header>
     <main class="flex-grow w-full p-4 mx-auto">
       <slot />
     </main>
